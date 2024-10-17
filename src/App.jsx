210334d082c7f8429afa215c0a99ca66baaaef7e
NadirSarsri <nadir.sarsri@gmail.com>
2024-10-17 11:49:30 +0100

    useState implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,61 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import Counters from "./components/counters";
 import Navbar from "./components/navbar";
 
-class App extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 2 },
-      { id: 2, value: 0 },
-      { id: 3, value: 3 },
-      { id: 4, value: 6 },
-    ],
-  };
-  handleIncrement = (counterId) => {
+const App = () => {
+  const [counters, setCounters] = useState([
+    { id: 1, value: 2 },
+    { id: 2, value: 0 },
+    { id: 3, value: 3 },
+    { id: 4, value: 6 },
+  ]);
+
+  const handleIncrement = (counterId) => {
     // console.log("Incrementing");
-    const counters = [...this.state.counters];
-    const counter = { ...this.state.counters.find((c) => c.id === counterId) };
-    const index = counters.findIndex((c) => c.id === counterId);
+    const copyCounters = [...counters];
+    const counter = { ...copyCounters.find((c) => c.id === counterId) };
+    const index = copyCounters.findIndex((c) => c.id === counterId);
     counter.value++;
-    counters[index] = counter;
-    this.setState({ counters });
+    copyCounters[index] = counter;
+    setCounters(copyCounters);
   };
-  handleDecrement = (counterId) => {
+  const handleDecrement = (counterId) => {
     // console.log("Decrementing");
-    const counters = [...this.state.counters];
-    const counter = { ...this.state.counters.find((c) => c.id === counterId) };
-    const index = counters.findIndex((c) => c.id === counterId);
+    const copyCounters = [...counters];
+    const counter = { ...copyCounters.find((c) => c.id === counterId) };
+    const index = copyCounters.findIndex((c) => c.id === counterId);
     counter.value--;
-    counters[index] = counter;
-    this.setState({ counters });
+    copyCounters[index] = counter;
+    setCounters(copyCounters);
   };
 
-  handleDelete = (counterId) => {
-    /* const index = this.state.counters.findIndex((c) => c.id === counterId);
-    console.log(index);
-    const copy = [...this.state.counters];
-    copy.splice(index, 1);
-      this.setState({ counters: copy }); */
-
-    const modified = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters: modified });
+  const handleDelete = (counterId) => {
+    const modified = counters.filter((c) => c.id !== counterId);
+    setCounters(modified);
   };
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
+  const handleReset = () => {
+    const modified = counters.map((c) => {
       return { ...c, value: 0 };
     });
-
-    this.setState({ counters });
+    setCounters(modified);
   };
 
-  render() {
-    const { counters } = this.state;
-    return (
-      <>
-        <Navbar
-          totalItems={counters.filter((counter) => counter.value > 0).length}
+  return (
+    <>
+      <Navbar
+        totalItems={counters.filter((counter) => counter.value > 0).length}
+      />
+      <main className="container pt-3">
+        <Counters
+          counters={counters}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          onReset={handleReset}
         />
-        <main className="container pt-3">
-          <Counters
-            counters={counters}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onReset={this.handleReset}
-          />
-        </main>
-      </>
-    );
-  }
-}
+      </main>
+    </>
+  );
+};
 
 export default App;
